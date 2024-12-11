1cadbd7ac1b7e1a0b47ecee2e0d99319b718912b
danielgoehring <69319848+danielgoehring@users.noreply.github.com>
2024-12-11 14:20:04 -0500

    fixed payment logic

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import image from './dogpicture.jpg';
-import AuthDetails from './components/AuthDetails';
-import Payment from './Payment'
-import Modal from './Modal';
-
+import image from "./dogpicture.jpg";
+import AuthDetails from "./components/AuthDetails";
+import Payment from "./Payment";
+import Modal from "./Modal";
 
 const Home = () => {
-
   const [payments, setPayments] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [name, setName] = useState("");
@@ -15,118 +13,156 @@ const Home = () => {
   const [amount, setAmount] = useState("");
   const [balance, setBalance] = useState(2000);
   const [isBelowZero, setisBelowZero] = useState(false);
- 
+
   useEffect(() => {
     if (balance < 0) {
-        setisBelowZero(true);
+      setisBelowZero(true);
+      setBalance(0);
     }
-}, [balance])
-  
-  
+  }, [balance]);
 
   const removePayment = (indexToRemove) => {
     setPayments(payments.filter((payment, index) => index !== indexToRemove));
   };
-  
 
   const makePayment = () => {
     setShowModal(true);
-    setBalance(2000);
+    // setBalance(2000);
     setisBelowZero(false);
   };
 
   const addPayment = () => {
-    
     setPayments([...payments, { name, reason, amount }]);
     setName("");
     setReason("");
     setShowModal(false);
     setAmount("");
-    setBalance(balance-amount);
-    
-    
+
+    setBalance(balance - amount);
   };
 
-    return (
-        <div className="main-container">
-            <div className="container-left">
-            <div className="img-name-ct">
-                <img src={image} alt="" className="profile-pic"/>
-                <div className="name-ct">
-                    <p style={{marginRight:'10px'}}>Hi,</p>
-                    <div>
-                    <AuthDetails />
-                    </div>
-
-                </div>
+  return (
+    <div className="main-container">
+      <div className="container-left">
+        <div className="img-name-ct">
+          <img src={image} alt="" className="profile-pic" />
+          <div className="name-ct">
+            <p style={{ marginRight: "10px" }}>Hi,</p>
+            <div>
+              <AuthDetails />
             </div>
-            <div className="money-ct">
-                <div className="m1">
-                    ${balance} in PaymentApp
-                </div>
-                <div className="transfer-m">
-                    Transfer Funds
-                </div>
-            </div>
-            <div className="hr">
-            
-            </div>
-            <div className="cred-container">
-                <div className="credit-card">
-                    <div className="cc">PaymentApp's Pay Later </div>
-                    <div className="cc2">Apply for opportunity</div>
-                </div>
-                <div className="learn-more">Learn more</div>
-            </div>
-            
-            <div className="button-ctn">
-                <button id="porButton" onClick={makePayment}>Make Payment</button>
-            </div>
+          </div>
         </div>
-        {showModal && (
+        <div className="money-ct">
+          <div className="m1">${balance} in PaymentApp</div>
+          <div className="transfer-m">Transfer Funds</div>
+        </div>
+        <div className="hr"></div>
+        <div className="cred-container">
+          <div className="credit-card">
+            <div className="cc">PaymentApp's Pay Later </div>
+            <div className="cc2">Apply for opportunity</div>
+          </div>
+          <div className="learn-more">Learn more</div>
+        </div>
+
+        <div className="button-ctn">
+          <button id="porButton" onClick={makePayment}>
+            Make Payment
+          </button>
+        </div>
+      </div>
+      {showModal && (
         <Modal onClose={() => setShowModal(false)}>
-            <div style={{textAlign: 'center'}}>
-                <h2>Make a Payment</h2>
+          <div style={{ paddingLeft: "20px", paddingRight: "20px" }}>
+            <div style={{ textAlign: "center", marginBottom: "25px" }}>
+              <h2 style={{ marginTop: "14px" }}>Make a Payment</h2>
             </div>
             <div>
-                <div>
-                    <span style={{ marginRight: '26px'}}>Name:</span>
-                    <input style={{color: 'white', width: '60%'}} value={name} onChange={e => setName(e.target.value)} />
-                </div>
-                <div>
-                    <span style={{marginRight: '12px'}}>Amount:</span> 
-                    <input style={{color: 'white', width: '60%'}} value={amount} onChange={e => setAmount(e.target.value)} />
-                </div>
-                <div>
-                    <span style={{ marginRight: '15px'}}>Reason:</span>
-                    <input style={{color: 'white', width: '60%'}} value={reason} onChange={e => setReason(e.target.value)} />
-                </div>
+              <div>
+                <span
+                  style={{
+                    marginRight: "26px",
+                    fontSize: "14px",
+                  }}
+                >
+                  Name:
+                </span>
+                <input
+                  style={{ color: "white", width: "94.5%", marginTop: "14px" }}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div>
+                <span style={{ marginRight: "12px", fontSize: "14px" }}>
+                  Amount:
+                </span>
+                <input
+                  style={{ color: "white", width: "94.5%", marginTop: "14px" }}
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                />
+              </div>
+              <div>
+                <span style={{ marginRight: "15px", fontSize: "14px" }}>
+                  Reason:
+                </span>
+                <input
+                  style={{ color: "white", width: "94.5%", marginTop: "14px" }}
+                  value={reason}
+                  onChange={(e) => setReason(e.target.value)}
+                />
+              </div>
             </div>
-            <div style={{textAlign: 'center'}}>
-                <button className="addPayment" onClick={addPayment}>Add Payment</button>
+            <div style={{ textAlign: "center", width: "100%" }}>
+              <button
+                className="addPayment"
+                onClick={() => {
+                  if (amount > balance) {
+                    alert("You don't have enough funds");
+                  } else {
+                    addPayment();
+                  }
+                }}
+                style={{ width: "50%" }}
+              >
+                Confirm
+              </button>
             </div>
-      </Modal>
+          </div>
+        </Modal>
       )}
-        <div className="container-right">
+      <div className="container-right">
         {payments.length === 0 ? (
-                    <div style={{textAlign: 'center'}}><p>Click "make payment" button to make payments...</p>
-                    
-                    </div>
-                    
-                ) : (
-                    <>
-                    {balance >= 0 && !isBelowZero ? (
-                    payments.map((payment, i) => (
-                        <Payment key={i} name={payment.name} reason={payment.reason} amount={payment.amount} onRemove={() => removePayment(i)} />
-                    ))
-                    ) : <div style={{textAlign: 'center'}}><p style={{color: '#d42424'}}>You don't have enough funds. Please try again.</p></div>}
-                    {/* {balance < 0 && (<div style={{textAlign: 'center'}}><p style={{color: '#d42424'}}>You don't have enough funds. Please try again.</p></div>)} */}
+          <div style={{ textAlign: "center" }}>
+            <p>Click "make payment" button to make payments...</p>
+          </div>
+        ) : (
+          <>
+            {balance >= 0 && !isBelowZero ? (
+              payments.map((payment, i) => (
+                <Payment
+                  key={i}
+                  name={payment.name}
+                  reason={payment.reason}
+                  amount={payment.amount}
+                  onRemove={() => removePayment(i)}
+                />
+              ))
+            ) : (
+              <div style={{ textAlign: "center" }}>
+                <p style={{ color: "#d42424" }}>
+                  You don't have enough funds. Please try again.
+                </p>
+              </div>
+            )}
+            {/* {balance < 0 && (<div style={{textAlign: 'center'}}><p style={{color: '#d42424'}}>You don't have enough funds. Please try again.</p></div>)} */}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-                    </>
-                )}
-        </div>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
--- src/Modal.js
+++ src/Modal.js
@@ -1,33 +1,48 @@
-import React from 'react';
+import React from "react";
 
 const Modal = ({ children, onClose }) => {
   return (
-    <div style={{ 
-      position: 'fixed', 
-      top: 0, 
-      left: 0, 
-      width: '100%', 
-      height: '100%', 
-      backgroundColor: 'rgba(0,0,0,0.5)', // Transparent gray background
-      display: 'flex', // Add flexbox layout to center the modal box
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <div style={{ 
-        display: 'flex', // Add flexbox layout to center the modal box
-        flexDirection: 'column',
-        
-        width: '80%', // Adjust to desired size
-        maxWidth: '400px', // Maximum width for mobile view
-        backgroundColor: 'rgb(44, 44, 44)', 
-        padding: '1em', 
-        borderRadius: '10px', // Rounded corners
-      }}>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(0,0,0,0.5)", // Transparent gray background
+        display: "flex", // Add flexbox layout to center the modal box
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex", // Add flexbox layout to center the modal box
+          flexDirection: "column",
+
+          width: "80%", // Adjust to desired size
+          maxWidth: "400px", // Maximum width for mobile view
+          backgroundColor: "rgb(44, 44, 44)",
+          padding: "1em",
+          borderRadius: "10px", // Rounded corners
+        }}
+      >
         {children}
-        <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-          <button style={{ marginTop: '16px' }} className='addPayment' onClick={onClose}>
-            Close
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            paddingLeft: "20px",
+            paddingRight: "20px",
+          }}
+        >
+          <button
+            style={{ marginTop: "16px", width: "50%", marginBottom: "22px" }}
+            className="addPayment"
+            onClick={onClose}
+          >
+            Cancel
           </button>
         </div>
       </div>
